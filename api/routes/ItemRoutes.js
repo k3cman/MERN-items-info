fb6c7645ab7a9f32093091be4e3332b09ce28bf2
k3cman <kecman.design@gmail.com>
2018-11-30 11:06:57 +0100

    main routes

--- api/routes/ItemRoutes.js
+++ api/routes/ItemRoutes.js
@@ -5,7 +5,7 @@ const _ = require("lodash");
 
 const conn = require("../../config/connection");
 
-router.get("/all-items", (req, res) => {
+router.get("/all", (req, res) => {
   conn.query(
     `SELECT * FROM enmon.article_collections`,
     (err, result, fields) => {
@@ -16,6 +16,38 @@ router.get("/all-items", (req, res) => {
         res.status(200).json(output);
       } else {
         res.status(404).json({ error: err });
+      }
+    }
+  );
+});
+
+router.get("/category/:cat", (req, res) => {
+  conn.query(
+    `SELECT id,title,brand,image FROM enmon.article_collections WHERE category in ('${
+      req.params.cat
+    }');`,
+    (err, result, fields) => {
+      let output = _.mapValues(_.groupBy(result, "brand"), list =>
+        list.map(article => _.omit(article, "brand"))
+      );
+      if (!err) {
+        res.status(200).json(output);
+      } else {
+        res.status(404).json({ error: err });
+      }
+    }
+  );
+});
+
+router.get("/brand/:brand", (req, res) => {
+  conn.query(
+    `SELECT id,title,image WHERE brand=?`,
+    [req.params.brand],
+    (err, result, fields) => {
+      if (!err) {
+        res.status(200).json(result);
+      } else {
+        res.status(400).json({ error: err });
       }
     }
   );
--- server.js
+++ server.js
@@ -1,34 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const conn = require('./config/connection');
+const bodyParser = require("body-parser");
+const conn = require("./config/connection");
 
 //Routes
-const ItemRoutes = require('./api/routes/ItemRoutes')
+const ItemRoutes = require("./api/routes/ItemRoutes");
 
 conn.connect(err => {
-    if(!err) {console.log('DB connection succeded')}else{
-        console.log('Db connection failed, Error:'+JSON.stringify(err , undefined, 2))
-    }
-})
+  if (!err) {
+    console.log("DB connection succeded");
+  } else {
+    console.log(
+      "Db connection failed, Error:" + JSON.stringify(err, undefined, 2)
+    );
+  }
+});
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use((req,res,next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers',
-    'Origin, X-Requested-with, Content-Type, Accept, Authorization')
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-with, Content-Type, Accept, Authorization"
+  );
 
-    if(req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methoids', 'PUT,POST,PATCH,DELETE,GET');
-        return res.status(200).json({});
-    }
-    next();
-})
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methoids", "PUT,POST,PATCH,DELETE,GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
-app.use('/items/', ItemRoutes)
+app.use("/collections/", ItemRoutes);
 
 const port = process.env.PORT || 5000;
-app.listen(port,() => {
-    console.log('App is running on port ' + port );
-})
+app.listen(port, () => {
+  console.log("App is running on port " + port);
+});
